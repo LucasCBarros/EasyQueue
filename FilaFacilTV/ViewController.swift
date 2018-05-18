e36fccdbcac2f3441cbffd90ff727cebc8561999
Lucas C. Barros <lucas_cb@icloud.com>
2018-05-18 15:16:35 -0300

    Funcionando Tableview TvOs

--- FilaFacilTV/ViewController.swift
+++ FilaFacilTV/ViewController.swift
@@ -9,48 +9,159 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var list = [1,2,3,4,5,6,7,8]
     
+    var openedQuestions:[Question] = []
+    var questionIDArray: [String] = []
+    var topTimer: Timer!
+    
+    
+    @IBOutlet weak var questionTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(QuestionService().getJsonFromUrl())
+//        print(QuestionService().getJsonFromUrl())
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // TODO: Test
+        getAllQuestions()
+//        getJsonFromUrl()
+        
+                topTimer = Timer.scheduledTimer(timeInterval: TimeInterval(3), target: self,
+                                                selector: #selector(ViewController.getJsonFromUrl),
+                                                userInfo: nil, repeats: true)
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    struct Question {
+        let questionID: String
+        let questionTitle: String
+        let requestedTeacher: String
+        let userID: String
+        let username: String
+        
+        init(json: [String: Any]) {
+            questionID = json["questionID"] as? String ?? "?"
+            questionTitle = json["questionTitle"] as? String ?? "??"
+            requestedTeacher = json["requestedTeacher"] as? String ?? "???"
+            userID = json["userID"] as? String ?? "????"
+            username = json["username"] as? String ?? "?????"
+        }
+    }
+    
+    @objc func getJsonFromUrl() {
+
+        
+        for index in 0..<self.questionIDArray.count {
+            
+            let URL: String = "https://filafacildev.firebaseio.com/Questions/\(self.questionIDArray[index]).json"
+            
+            guard let url = NSURL(string: URL) else { return }
+            URLSession.shared.dataTask(with: (url as? URL)!, completionHandler: {(data, response, error) in
+    
+                guard let data = data else { return }
+    
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+    
+                    let question = Question(json: json)
+                    self.openedQuestions.append(question)
+                    print(">>>>>>>>>>", question.username)
+                    
+    
+                } catch let jsonErr {
+                    print("Error serializing json:", jsonErr)
+                }
+            }).resume()
+        }
+        print("---------")
+        if(questionIDArray.count == openedQuestions.count) {
+            self.questionTableView.reloadData()
+            
+        }
+    }
+    
+    @objc func getAllQuestions() {
+        let URL: String = "https://filafacildev.firebaseio.com/Questions.json"
+        
+        //creating a NSURL
+        guard let url = NSURL(string: URL) else { return }
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url as? URL)!, completionHandler: {(data, response, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+                
+                let jsonString = ("\(json.keys)")
+//                print("JSON:", jsonString)
+                
+                let jsonData = jsonString.data(using: .utf8)! // Conversion to UTF-8 cannot fail.
+                
+                if let array = (try? JSONSerialization.jsonObject(with: jsonData, options: [])) as? [String] {
+                    self.questionIDArray.removeAll()
+                    // Let's dump the array for demonstration purposes:
+//                    print("\nArray:")
+                    for (idx, elem) in array.enumerated() {
+//                        print(idx, elem)
+                        self.questionIDArray.append(elem)
+                    }
+//                    print(">>>>>>>>", self.questionIDArray)
+                    
+                    self.getJsonFromUrl()
+                    
+                } else {
+                    print("malformed input")
+                }
+                
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+        }).resume()
+        
+        questionTableView.reloadData()
+    }
 }
 
 // MARK: - TableView functions
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // Number of cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.openedQuestions.count
+        return openedQuestions.count
+    }
+    
+    // Shows tableView cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // Number of cells
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return list.count
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell") as? QuestionTableViewCell
         
-        // Shows tableView cells
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if(self.openedQuestions.count > 0) {
+            cell?.profileName.text = self.openedQuestions[indexPath.row].username
+            cell?.questionLabel.text = "\(indexPath.row+1)"
+            cell?.numberLabel.text = self.openedQuestions[indexPath.row].requestedTeacher
             
-            let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell") as? QuestionTableViewCell
-            cell?.profileName.text = "Name"
-            cell?.questionLabel.text = "\(self.list[indexPath.row])"
-            cell?.numberLabel.text = "\(self.list[indexPath.row])"
             
-            return cell!
         }
         
-        // Delete cell and update student status in Firebase
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        }
-        
-        // Allows to edit cell according to profile type
-//        func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//            return true
-//        }
+        return cell!
+    }
+    
+    // Delete cell and update student status in Firebase
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    }
     
 }
 
