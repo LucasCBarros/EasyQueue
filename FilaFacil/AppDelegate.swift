adbeb94a64345306234ac7a7d34cfc420ac7f01b
Lucas C. Barros <lucas_cb@icloud.com>
2018-05-18 17:47:43 -0300

    CB - Sobr

--- FilaFacil/AppDelegate.swift
+++ FilaFacil/AppDelegate.swift
@@ -15,64 +15,132 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
-
+    
     var window: UIWindow?
-
+    
     override init() {
+        super.init()
         FirebaseApp.configure()
-        Database.database().isPersistenceEnabled = true
         
+        Messaging.messaging().delegate = self
     }
-
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.sharedManager().enable = true
-
-//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(isGranted, err) in
-//            if err != nil {
-//                // Something went wront
-//                print("HELP")
-//            } else {
-//                UNUserNotificationCenter.current().delegate = self
-//                Messaging.messaging().delegate = self
-//                
-//                DispatchQueue.main.async {
-//                    application.registerForRemoteNotifications()
-////                    FirebaseApp.configure()
-//                }
-//            }
-//        })
-//        let token = InstanceID.instanceID().token()
-//        print(token!)
-    
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        Messaging.messaging().shouldEstablishDirectChannel = true
+        NotificationCenter.default.addObserver(self, selector: #selector(self.tokenRefreshNotification), name: NSNotification.Name.InstanceIDTokenRefresh, object: nil)
+        
         return true
     }
     
-    func connect() {
-        Messaging.messaging().shouldEstablishDirectChannel = true
-    }
-    
-    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
-        let newToken = InstanceID.instanceID().token()
-        print(newToken)
-        connect()
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        Messaging.messaging().shouldEstablishDirectChannel = false
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
-
     func applicationDidBecomeActive(_ application: UIApplication) {
-        connect()
+        print("applicationDidBecomeActive \(Messaging.messaging().shouldEstablishDirectChannel)")
+        Messaging.messaging().connect { error in
+            print(error)
+        }
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        Messaging.messaging().appDidReceiveMessage(userInfo)
     }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print(userInfo)
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map({ data -> String in
+            return String(format: "%02.2hhx", data)
+        })
+        
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        // Print full message.
+        print(userInfo)
+        
+        // Change this to your preferred presentation option
+        completionHandler([])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        print(userInfo)
+        
+        completionHandler()
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+        
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    // [END refresh_token]
+    // [START ios_10_data_message]
+    // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
+    // To enable direct data messages, you can set Messaging.messaging().shouldEstablishDirectChannel to true.
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        print("Received data message: \(remoteMessage.appData)")
+    }
+    
+    @objc func tokenRefreshNotification(notification: NSNotification) {
+        if let refreshedToken = InstanceID.instanceID().token() {
+            print("InstanceID token: \(refreshedToken)")
+            
+            //            User.getIDTokenForcingRefresh(refreshedToken.)
+        }
+    }
+    
+    //    func connect() {
+    //        Messaging.messaging().shouldEstablishDirectChannel = true
+    //    }
+    
+    //    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+    //        let newToken = InstanceID.instanceID().token()
+    //        print(newToken)
+    //        connect()
+    //    }
+    
+    //    func applicationDidEnterBackground(_ application: UIApplication) {
+    //        Messaging.messaging().shouldEstablishDirectChannel = false
+    //    }
+    
+    //    func applicationDidBecomeActive(_ application: UIApplication) {
+    //        connect()
+    //    }
+    
 }
