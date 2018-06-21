fd887cd350c5a91d611d1c86e3070de57e8dcb97
Luan Sobreira <luan.seo@puccampinas.edu.br>
2018-06-21 17:44:15 -0300

    Finalização do merge e fix do plist

--- FilaFacil/AppDelegate.swift
+++ FilaFacil/AppDelegate.swift
@@ -15,9 +15,6 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
-    func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
-        //
-    }
     
     
     func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
