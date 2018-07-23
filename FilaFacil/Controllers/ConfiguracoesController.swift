1c94aff201b86d7911b52e0155fcf3030bb34c06
Ellen Mota <ellenmota29@gmail.com>
2018-07-23 15:44:25 -0300

    Função que leva para notificações funcionando

--- FilaFacil/Controllers/ConfiguracoesController.swift
+++ FilaFacil/Controllers/ConfiguracoesController.swift
@@ -11,9 +11,29 @@ import UIKit
 class ConfiguracoesController: UIViewController {
 
     
+    @IBOutlet weak var switchDev: UISwitch!
+    @IBOutlet weak var switchDesign: UISwitch!
+    @IBOutlet weak var switchBusiness: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    
+    
+    //Leva para Notificações em configurações
+    @IBAction func goNotification(_ sender: UIButton) {
+        
+        guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                print("Settings opened: \(success)") // Prints true
+            })
+        }
+        
+    }
+    
 }
